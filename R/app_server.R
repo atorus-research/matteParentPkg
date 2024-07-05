98d6247e05fd4024f213a0f5ace7e80593e2d9bb
Jiwan Heo <jiwanheo123@gmail.com>
2024-07-05 15:57:22 +0000

    dependencies

--- R/app_server.R
+++ R/app_server.R
@@ -21,7 +21,7 @@ app_server <- function(input, output, session) {
   }
 
   metadata <- yaml::read_yaml("jobs/meta.yaml")
-  metadata$data <- map(metadata$data, as.symbol)
+  metadata$data <- purrr::map(metadata$data, as.symbol)
   #metadata$modules$plot <- map(metadata$modules$plot, as.symbol)
 
   data <- readRDS("jobs/data.rds")
